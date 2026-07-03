9c32411e285eee0c5171c96b1c274b15529736a9
tarcisio <tarcisio.genaro@gmail.com>
2026-07-03 18:09:54 +0000

    Tweak it

--- home-modules/zsh-shell.nix
+++ home-modules/zsh-shell.nix
@@ -54,12 +54,16 @@ in
       enable = true;
       defaultCommand = "${fd} -H --type f";
       defaultOptions = [ "--height 50%" ];
-      fileWidget.command = "${defaultCommand}";
-      fileWidget.options = [
-        "--preview '${lib.getExe pkgs.bat} --color=always --plain --line-range=:200 {}'"
-      ];
-      changeDirWidget.command = "${fd} -H --type d";
-      changeDirWidget.options = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+      fileWidget = {
+        command = "${defaultCommand}";
+        options = [
+          "--preview '${lib.getExe pkgs.bat} --color=always --plain --line-range=:200 {}'"
+        ];
+      };
+      changeDirWidget = {
+        command = "${fd} -H --type d";
+        options = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+      };
       historyWidget.options = [ ];
     };
 
