6f54f62a7c8b58ac32388a50d6fc4170157e20fd
tarcisio <tarcisio.genaro@gmail.com>
2026-07-26 15:30:49 +0000

    Remove CACHIX_AUTH_TOKEN

--- home-configurations/tarci.nix
+++ home-configurations/tarci.nix
@@ -20,7 +20,6 @@ in
     let
       githubToken = "$(${pkgs.coreutils-full}/bin/cat ${config.age.secrets.GITHUB_TOKEN.path})";
       codebergToken = "$(${pkgs.coreutils-full}/bin/cat ${config.age.secrets.CODEBERG_TOKEN.path})";
-      cachixAuthToken = "$(${pkgs.coreutils-full}/bin/cat ${config.age.secrets.CACHIX_AUTH_TOKEN.path})";
       tarcDevenvNixpkgsRollingGenericToken = "$(${pkgs.coreutils-full}/bin/cat ${config.age.secrets.TARC_DEVENV_NIXPKGS_ROLLING_GENERIC_TOKEN.path})";
       tarcDevenvNixpkgsRollingPlayConanToken = "$(${pkgs.coreutils-full}/bin/cat ${config.age.secrets.TARC_DEVENV_NIXPKGS_ROLLING_PLAY_CONAN_TOKEN.path})";
     in
@@ -32,7 +31,6 @@ in
         sessionVariables = {
           GITHUB_TOKEN = githubToken;
           CODEBERG_TOKEN = codebergToken;
-          CACHIX_AUTH_TOKEN = cachixAuthToken;
           TARC_DEVENV_NIXPKGS_ROLLING_GENERIC_TOKEN = tarcDevenvNixpkgsRollingGenericToken;
           TARC_DEVENV_NIXPKGS_ROLLING_PLAY_CONAN_TOKEN = tarcDevenvNixpkgsRollingPlayConanToken;
           EDITOR = "vim";
@@ -82,8 +80,6 @@ in
         "GITHUB_TOKEN".file = "${inputs.secrets}/nixpkgs-review-github-pat.age";
 
         "CODEBERG_TOKEN".file = "${inputs.secrets}/codeberg-pat.age";
-
-        "CACHIX_AUTH_TOKEN".file = "${inputs.secrets}/tarcisio-system-flakes-auth-token.age";
 
         "TARC_DEVENV_NIXPKGS_ROLLING_GENERIC_TOKEN".file =
           "${inputs.secrets}/tarc-devenv-nixpkgs-rolling-generic-auth-token.age";
